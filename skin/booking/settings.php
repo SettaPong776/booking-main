7726eb956022a7144b39d36eb4320c488a7511be
SettaPong776 <103419893+SettaPong776@users.noreply.github.com>
2026-03-17 13:12:46 +0700

    first commit

---
+++ skin/booking/settings.php
@@ -0,0 +1,16 @@
+<?php
+/* Theme default settings */
+return array(
+    'name' => 'Booking',
+    'header_bg_color' => '#1e3a5f',
+    'warpper_bg_color' => '#f0f4f8',
+    'content_bg' => '#FFFFFF',
+    'header_color' => '#FFFFFF',
+    'footer_color' => '#6b7280',
+    'logo_color' => '#1e3a5f',
+    'login_header_color' => '#1e3a5f',
+    'login_footer_color' => '#6b7280',
+    'login_color' => '#1e1e2e',
+    'login_bg_color' => '#e8edf5',
+    'theme_width' => 'wide'
+);
